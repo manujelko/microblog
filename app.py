f19c5db5d01d9fcaab884bec173152fe9db730e7
Manuel Šarić <manujelko@gmail.com>
2022-09-29 18:13:52 +0200

    Storing Microblog entries in MongoDB

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import datetime
 from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
-
+client = MongoClient("mongodb://localhost:27017/test")
+db = client.microblog
+app.db = client.microblog
 entries = []
 
 @app.route("/", methods=["GET", "POST"])
@@ -11,6 +14,7 @@ def home():
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
         entries.append((entry_content, formatted_date))
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
     
     entries_with_date = [
         (
